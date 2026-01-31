20d8c8d42bd530e24f06013c3aaa5f297c5b6da5
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-01-30 22:15:51 -0300

    tipos de datos php

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,57 @@
 <?php include 'includes/header.php';
 
+// Strings
+$nombre = "Ignacio";
+$apellido = 'Dorigo';
+echo "Hola $nombre $apellido";
 
+// Ints
+$edad = 25;
+$anio = -2024;
+
+// Floats
+$precio = 99.99;
+$pi = 3.14;
+
+// booleanss
+$activo = true;
+$esAdmin = false;
+
+// Arrays
+$numeros = [1, 2, 3, 4];
+
+// Array asociativo (objet)
+$persona = [
+    "nombre" => "Ignacio",
+    "edad" => 25,
+    "activo" => true
+];
+echo $persona["nombre"];
+
+// Funciones que se puedeb pasar como parametro (callbacks)
+function saludar()
+{
+    echo "Hola";
+}
+
+$funcion = "saludar";
+$funcion();
+
+
+// Null
+$valor = null;
+
+// Resource (para archivos externos, BD, SOCKETS)
+$archivo = fopen("test.txt", "r");
+
+// Clases de objetos
+class Usuario
+{
+    public $nombre;
+}
+
+$u = new Usuario();
+$u->nombre = "Ignacio";
 
 
 include 'includes/footer.php';
