7a73f08a90fb94697f328d36ce34cea3d7ded294
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-02-01 20:31:38 -0300

    require,include

--- 17-require.php
+++ 17-require.php
@@ -1,6 +1,10 @@
 <?php include 'includes/header.php';
 
+// Sirven para importar un archivo php en otro archivo php
 
+// INCLUDE --> Si el archivo no existe, me tira un warning, pero no detiene el script
+
+// REQUIRE --> Si el archivo no existe, tira un error fatal, detiene el script
 
 
 
