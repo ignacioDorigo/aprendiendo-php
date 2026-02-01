f061596e778439f289e202dc99ecbe4a6500738f
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-02-01 15:50:51 -0300

    loops

--- 13-loops.php
+++ 13-loops.php
@@ -1,6 +1,47 @@
 <?php include 'includes/header.php';
 
 
+// Loops en PHP
 
+
+// For each en asociativos
+$frutas = ["manzana", "banana", "pera"];
+
+foreach ($frutas as $fruta) {
+    echo $fruta . "<br>";
+}
+
+
+// For each en indexados
+$persona = ["nombre" => "Ana", "edad" => 25];
+
+foreach ($persona as $clave => $valor) {
+    echo "$clave: $valor<br>";
+}
+
+
+// For con indices
+for ($i = 0; $i < count($frutas); $i++) {
+    echo $frutas[$i] . "<br>";
+}
+
+
+
+// While
+$i = 0;
+
+while ($i < 3) {
+    echo $i;
+    $i++;
+}
+
+
+// Do While
+$i = 5;
+
+do {
+    echo $i;
+    $i++;
+} while ($i < 3);
 
 include 'includes/footer.php';
