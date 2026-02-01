0423ec68839cee23453098896cb264018aa8fc61
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-02-01 19:01:45 -0300

    funciones

--- 15-funciones.php
+++ 15-funciones.php
@@ -1,7 +1,24 @@
 <?php include 'includes/header.php';
+// Observacion: Es muy similar a JS, con poquitos cambios en la sintaxis
+// Funcion 1
+function sumar($a, $b)
+{
+    return $a + $b;
+}
+
+// Funcion 2
+$suma = function ($a, $b) {
+    return $a + $b;
+};
+
+// Funciones 3, son funciones flecha
+$doble = fn($n) => $n * 2;
 
 
-
-
+// Mas ejemplos
+function sumar22($a = 3, $b = 2)
+{
+    return $a + $b;
+};
 
 include 'includes/footer.php';
