98deb238044a8afbe9a6b5f459eedc7dea32610e
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-02-01 20:56:56 -0300

    json_decode y encode

--- 18.json.php
+++ 18.json.php
@@ -1,5 +1,35 @@
 <?php include 'includes/header.php';
 
+// JSON ENCODE --> convierte PHP a JSON/STRING
+
+// JSON DECODE --> convierte JSON/STRING a PHP
+
+
+// --- EJEMPLO DE ENCODE/CODIFICAR ---
+// enviar $json a una API
+$usuario = [
+    "nombre" => "Ignacio",
+    "edad" => 25
+];
+
+$json = json_encode($usuario,JSON_UNESCAPED_UNICODE);
+
+
+
+// --- EJEMPLO DE DECODE/DECODIFICAR ---
+
+// Recibir daos de una API
+$respuesta = file_get_contents("https://api.ejemplo.com/user/1");
+$datos = json_decode($respuesta, true);
+echo $datos["nombre"];
+
+// Recibir daos de una API 
+// yo recibiria esto
+// {"tema":"oscuro","idioma":"es"} 
+
+$config = json_decode($fila["config"], true);
+echo $config["tema"];
+
 
 
 
