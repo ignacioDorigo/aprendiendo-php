ed79fd09d40c0271934450310b59ae32f6f1a594
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-02-01 00:42:36 -0300

    arrays indexados

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,35 @@
 <?php include 'includes/header.php';
 
+// Son los tipicos arrays indexados
+$numeros = [1, 2, 3, 4, 5];
+// echo $numeros[0];
 
 
+
+// CREAR ARRAY -> Forma corta
+$frutas = ["Manzana", "Banana", "Naranja"];
+
+// CREAR ARRAY -> Forma clásica
+$frutas = array("Manzana", "Banana", "Naranja");
+
+// LEN de aray
+// echo count($frutas);
+
+
+foreach ($frutas as $fruta) {
+    echo "<p>" . $fruta . "</p>";
+}
+
+
+
+
+array_push($frutas, "Pera");
+array_pop($frutas);
+array_shift($frutas);
+array_unshift($frutas, "Kiwi");
+// Verifica si un elemento existe en un array
+if (in_array("Banana", $frutas)) {
+    echo "Existe!";
+}
 
 include 'includes/footer.php';
