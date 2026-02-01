868cafc9a11bef5dc2fa4789335eda5b336665d4
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-02-01 13:19:32 -0300

    metodos arrays, if-elseif-switch

--- 11-funciones-arrays.php
+++ 11-funciones-arrays.php
@@ -1,6 +1,28 @@
 <?php include 'includes/header.php';
 
 
+// In array
+$carrito = ["tablet","play","television"];
+var_dump(in_array("play",$carrito));
+
+// Sort, asort, rsort, ksort, todos para ordenar distintas cosas
+
+// Largo del array
+count($carrito);
+
+// Elimina el ultimo
+array_pop($carrito);
+
+// Agrega al final
+array_push($carrito,"Kiwi");
+
+// Tambien existe el merge
+// array_merge(array1,array2);
+
+// Devuelve clave o indice de un valor. (similar a un indexOff, pero tambien puede devolver un clave aparte de un indice)
+// array_search(elementoABuscar,dondeBuscarlo);
+array_search("banana", ["manzana", "banana", "pera"]);
+// Devuelve ---> 1
 
 
 
--- 12-condicionales.php
+++ 12-condicionales.php
@@ -1,6 +1,33 @@
 <?php include 'includes/header.php';
 
+// If como siempre
+// Existe el elif como else if.
+$nota = 7;
 
+if ($nota >= 9) {
+    echo "Excelente";
+} elseif ($nota >= 6) {
+    echo "Aprobado";
+} else {
+    echo "Desaprobado";
+}
+
+// Switch es igual que en JS
+// condicinoa a evaluar, casos, todos con breaks, y default
+$dia = "lunes";
+
+switch ($dia) {
+    case "lunes":
+        echo "Arranca la semana";
+        break;
+
+    case "viernes":
+        echo "Se viene el finde";
+        break;
+
+    default:
+        echo "Día normal";
+}
 
 
 
