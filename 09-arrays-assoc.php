646e4bbcdf06f2aa495482ac8030e18cd6f926b6
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-02-01 00:29:44 -0300

    strings, arrays asociativos

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,42 @@
 <?php include 'includes/header.php';
 
 
+// Es similar a au n objeto literal en SJ;
+
+$persona = [
+    "nombre" => "Ana",
+    "edad" => 25,
+    "ciudad" => "Buenos Aires"
+];
+
+// Acceder a un valor, con su clave
+echo $persona["nombre"]; // Ana
+
+$claves = array_keys($persona);
+print_r($claves); // ["nombre","edad","ciudad"]
+
+$valores = array_values($persona);
+print_r($valores); // ["Ana", 25, "BA"]
+
+
+// Verifica si un objeto tiene ese valor (no key) 
+if (in_array("Ana", $persona)) {
+    echo "Existe!";
+}
+
+// Verifica si un objeto tiene una key
+if (array_key_exists("edad", $persona)) {
+    echo "La clave edad existe";
+}
+
+// Elimina un elemento por su clave
+unset($persona["ciudad"]);
+print_r($persona); // ["nombre"=>"Ana","edad"=>25]
+
+// Recorre un array asociativo
+foreach ($persona as $clave => $valor) {
+    echo "$clave => $valor\n";
+}
 
 
 include 'includes/footer.php';
